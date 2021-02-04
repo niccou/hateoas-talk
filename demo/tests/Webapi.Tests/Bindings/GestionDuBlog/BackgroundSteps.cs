f0b8af744166d2b79b6e9734c7de4c2ef20fdc60
Nicolas Cousin <niccoudev@gmail.com>
2021-02-04 23:10:23 +0100

    test(Blog): ajout du premier cas d'usage de lecture anonyme

---
+++ demo/tests/Webapi.Tests/Bindings/GestionDuBlog/BackgroundSteps.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using WebApi.Core.Blog;
+using WebApi.Core.Models;
+using WebApi.Tests.Shared;
+
+namespace WebApi.Tests.Bindings.GestionDuBlog
+{
+    public class BackgroundSteps : Context
+    {
+        protected BackgroundSteps(ScenarioContext context, ApiServer server) : base(context, server) { }
+
+        private Mock<IGetBlog> BlogMock { get; } = new Mock<IGetBlog>();
+
+        [Given("Je démarre l'api avec les posts suivant")]
+        public void GivenJeDemarreLApiAvecLesPostsSuivant(Table table)
+        {
+            var posts = CreatePostsCollection(table);
+
+            BlogMock.Setup(_ => _.Get()).Returns(new Blog
+            {
+                Posts = posts
+            });
+
+            Server!.ConfigureWebHost = builder =>
+                builder.ConfigureTestServices(services =>
+                    services.AddTransient(_ => BlogMock.Object));
+        }
+
+        private static IReadOnlyCollection<Post> CreatePostsCollection(Table table)
+            => table.CreateSet(r => new Post
+            {
+                Title = r["Title"],
+                Description = r["Description"],
+                State = Enum.Parse<Post.PostState>(r["State"], true),
+                Author = new Author { Name = r["Author"] }
+            })
+            .ToList();
+    }
+}
---
+++ demo/tests/Webapi.Tests/Features/GestionDuBlog.feature
@@ -0,0 +1,18 @@
+﻿Feature: Gestion du blog
+
+Background: J'ai une liste de posts au démarrage de l'api
+	Given Je démarre l'api avec les posts suivant
+		| Author          | State     | Title                                         | Description                                                 |
+		| Andy Bloch      | Draft     | New Book: Life Lessons: Hold’em Poker Style   | Lorem ipsum dolor sit amet.                                 |
+		| Andy Bloch      | Published | Poker and Blackjack training available        | Sed vel odio consequat nunc viverra mollis.                 |
+		| Daniel Negreanu | Draft     | The WSOP POY Oopsie!                          | Phasellus a est sed tellus blandit cursus mollis eget odio. |
+		| Daniel Negreanu | Published | Should We Care if People in the US use a VPN? | Vivamus eu faucibus erat.                                   |
+
+Scenario: En tant qu'anonyme je ne peux voir que la liste des
+	Given Je suis anonyme
+	When Je veux voir le blog
+	Then Je recois une liste de posts
+	And Je vois uniquement les posts
+		| Author          | Title                                         | Description                                 |
+		| Andy Bloch      | Poker and Blackjack training available        | Sed vel odio consequat nunc viverra mollis. |
+		| Daniel Negreanu | Should We Care if People in the US use a VPN? | Vivamus eu faucibus erat.                   |
